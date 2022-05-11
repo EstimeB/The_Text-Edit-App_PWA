b2d553fa70f2e44652c71ff505c766675fc20e8d
brith <bgc@briths-MacBook-Air.local>
2022-05-11 16:16:10 -0700

    filled in the TODO under database.js.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+   // Create connection to database
+   const jateDb = await openDB('jate');
+
+   // Use .add to store and pass in the content
+   const request = jateDb.add({ content });
+ 
+   // Get confirmation of the request
+   const result = await request;
+   console.log('content saved', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
 
+  // Create connection to database
+  const jateDb = await openDB('jate');
+
+  // Use .getAll() method to get all the content
+  const request = jateDb.getAll();
+
+  // Get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+
+// Start the database.
 initdb();
